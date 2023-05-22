fa48403b7e82e377fef5524da02cb82775a4c289
Majoros-Peter <70958388+Majoros-Peter@users.noreply.github.com>
2023-05-22 23:58:48 +0200

    Kész :)

---
+++ Operatorok/Kifejezes.cs
@@ -0,0 +1,51 @@
+﻿namespace Operatorok
+{
+    internal class Kifejezes
+    {
+        public int ASzam { get; private set; }
+        public string Operator { get; private set; }
+        public int BSzam { get; private set; }
+
+        public Kifejezes(string[] egyenlet)
+        {
+            ASzam = Convert.ToInt32(egyenlet[0]);
+            Operator = egyenlet[1];
+            BSzam = Convert.ToInt32(egyenlet[2]);
+        }
+        public static string[] HasznalhatoOperatorok { get => new string[] { "+", "-", "*", "/", "div", "mod" }; }
+
+        public static string KifejezesMeghatarozasa(Kifejezes kifejezes)
+        {
+            string output;
+
+            switch (kifejezes.Operator)
+            {
+                case "+":
+                    output = $"{kifejezes.ASzam + kifejezes.BSzam}";
+                    break;
+                case "-":
+                    output = $"{kifejezes.ASzam - kifejezes.BSzam}";
+                    break;
+                case "*":
+                    output = $"{kifejezes.ASzam * kifejezes.BSzam}";
+                    break;
+                case "/":
+                    output = kifejezes.ASzam == 0 || kifejezes.BSzam == 0 ? "Egyéb hiba" : $"{kifejezes.ASzam * 1.0 / kifejezes.BSzam}";
+                    break;
+                case "div":
+                    output = kifejezes.ASzam == 0 || kifejezes.BSzam == 0 ? "Egyéb hiba" : $"{kifejezes.ASzam / kifejezes.BSzam}";
+                    break;
+                case "mod":
+                    output = kifejezes.ASzam == 0 || kifejezes.BSzam == 0 ? "Egyéb hiba" : $"{kifejezes.ASzam % kifejezes.BSzam}";
+                    break;
+                default:
+                    output = "Hibás operátor!";
+                    break;
+            }
+
+            return $"{kifejezes} = {output}";
+        }
+
+        public override string ToString() => $"{ASzam} {Operator} {BSzam}";
+    }
+}
--- Operatorok/Program.cs
+++ Operatorok/Program.cs
@@ -4,6 +4,22 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        
+        List<Kifejezes> kifejezesek = File.ReadAllLines("../../../kifejezesek.txt").Select(G => new Kifejezes(G.Split())).ToList();
+
+        Console.WriteLine($"2. feladat: Kifejezések száma: {kifejezesek.Count}");
+        Console.WriteLine($"3. feladat: Kifejezések maradékos osztással: {kifejezesek.Count(G=>G.Operator=="mod")}");
+        Console.WriteLine($"4. feladat: {(kifejezesek.Any(G=>G.ASzam%10 == 0 && G.BSzam%10 == 0) ? "Van" : "Nincs")} ilyen kifejezés!");
+        Console.WriteLine($"5. feladat: Statisztika");
+        kifejezesek.Where(G => Kifejezes.HasznalhatoOperatorok.Contains(G.Operator)).GroupBy(G => G.Operator).ToList().ForEach(G => Console.WriteLine($"\t{G.Key} -> {G.Count()}"));
+        string kifejezes = "";
+        while (kifejezes != "vége")
+        {
+            Console.Write($"7. feladat: Kérek egy kifejezést (pl.: 1 + 1): ");
+            kifejezes = Console.ReadLine() ?? "vége";
+            if(kifejezes != "vége")
+                Console.WriteLine(Kifejezes.KifejezesMeghatarozasa(new(kifejezes.Split())));
+        };
+        File.WriteAllLines("../../../eredmenyek.txt", kifejezesek.Select(G=> Kifejezes.KifejezesMeghatarozasa(G)));
+        Console.WriteLine($"8. feladat: eredmenyek.txt");
     }
 }
